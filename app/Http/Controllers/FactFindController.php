cea689407191995eb4b44743d953e3b52c6d844b
genaquinoo <genetess.aquino@wealthatwork.co.uk>
2024-01-03 15:20:25 +0000

    Acoomplished subtickets AH-40 and AH-43

--- app/Http/Controllers/FactFindController.php
+++ app/Http/Controllers/FactFindController.php
@@ -13,13 +13,128 @@ class FactFindController extends Controller
     {
         $this->clientRepository = $cr;
     }
-    public function show($client, Request $request)
+    public function show($client, Request $request) //fact-find?step=1&section=6
     {
+        $tabs = [
+            [
+                'name' => 'Basic Details',
+                'href' => '#',
+                'current' => true,
+                'progress' => 30,
+                'sidebaritems' => [
+                    [
+                        'name' => 'Personal Details',
+                        'href' => '#',
+                        'current' => true
+                    ],
+                    [
+                        'name' => 'Health Details',
+                        'href' => '#',
+                        'current' => false
+                    ],
+                    [
+                        'name' => 'Address and contact details',
+                        'href' => '#',
+                        'current' => false
+                    ],
+                    [
+                        'name' => 'Family',
+                        'href' => '#',
+                        'current' => false
+                    ],
+                    [
+                        'name' => 'Employment details',
+                        'href' => '#',
+                        'current' => false
+                    ]
+                ]
+            ],
+            [
+                'name' => 'Income and Expenditure',
+                'href' => '#',
+                'current' => false,
+                'progress' => 100,
+                'sidebaritems' => [
+                    [
+                        'name' => 'X1',
+                        'href' => '#',
+                        'current' => true
+                    ],
+                    [
+                        'name' => 'X2',
+                        'href' => '#',
+                        'current' => false
+                    ]
+                ]
+            ],
+            [
+                'name' => 'Assets',
+                'href' => '#',
+                'current' => false,
+                'progress' => 80,
+                'sidebaritems' => [
+                    [
+                        'name' => 'Y1',
+                        'href' => '#',
+                        'current' => true
+                    ],
+                    [
+                        'name' => 'Y2',
+                        'href' => '#',
+                        'current' => false
+                    ],
+                    [
+                        'name' => 'Y3',
+                        'href' => '#',
+                        'current' => false
+                    ]
+                ]
+            ],
+            [
+                'name' => 'Liabilities',
+                'href' => '#',
+                'current' => false,
+                'progress' => 0,
+                'sidebaritems' => [
+                    [
+                        'name' => 'Z1',
+                        'href' => '#',
+                        'current' => true
+                    ],
+                    [
+                        'name' => 'Z2',
+                        'href' => '#',
+                        'current' => false
+                    ]
+                ]
+            ],
+            [
+                'name' => 'Risk',
+                'href' => '#',
+                'current' => false,
+                'progress' => 10,
+                'sidebaritems' => [
+                    [
+                        'name' => 'W1',
+                        'href' => '#',
+                        'current' => true
+                    ]
+                ]
+            ],
+            [
+                'name' => 'Objectives',
+                'href' => '#',
+                'current' => false,
+                'progress' => 25
+            ]
+        ];
+
         return Inertia::render('FactFind',[
             'title' => 'Fact Find',
             'breadcrumbs' => $this->clientRepository->loadBreadcrumbs(),
             'step' =>  $request->step,
-            'section' => $request->section
+            'section' => $request->section,
+            'tabs' => $tabs
         ]);
     }
 }
---
+++ resources/js/Components/Tab.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { defineProps, inject, provide } from "vue";
+
+const props = defineProps({
+    tab: {
+        type: Object
+    },
+    tabIndex: {
+        type: Number
+    }
+});
+
+const selectedTitle = inject("selectedTitle");
+
+provide("tabIndex", props.tabIndex);
+
+</script>
+
+<template>
+    <div class="bg-aaron-900 sm:rounded-lg p-8" v-show="tab.name == selectedTitle">
+        <div class="my-6 pl-6">
+            <span class="text-2xl font-medium">{{ tab.name }}</span>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+</style>
