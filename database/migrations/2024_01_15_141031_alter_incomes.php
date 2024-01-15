e0e7a1f344ed7bf71fff9b4c5774f00d3d2c01ac
Harry Messenger <harrymessenger@Harrys-MacBook-Air.local>
2024-01-15 14:49:02 +0000

    wip radio

---
+++ database/migrations/2024_01_15_141031_alter_incomes.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('incomes', function (Blueprint $table) {
+            $table->integer('net_amount')->after('gross_amount')->nullable();
+            $table->integer('expenses')->after('frequency')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('incomes', function (Blueprint $table) {
+            $table->dropColumn('occupation');
+        });
+    }
+};
