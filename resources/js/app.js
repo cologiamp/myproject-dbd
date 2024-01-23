09f41638cf2169c3669ab2b9d52fe5eba35c6bdb
Harry Messenger <harrymessenger@Harrys-MacBook-Air.local>
2024-01-23 07:24:55 +0000

    remove ray from fe

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,6 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
-import { RayPlugin } from 'vue-ray';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -16,13 +15,6 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
-            .use(RayPlugin, {
-                interceptErrors: true,
-                port: 23517,
-                nodeRaySettings: {
-                    interceptConsoleLog: true,
-                },
-            })
             .mount(el);
     },
     progress: {
