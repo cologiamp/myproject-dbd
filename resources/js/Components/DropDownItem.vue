7ee27a9f8045259e2662248f454a415d2a748ac6
robertjurewiczwaw <ignacio.giampaoli@wealthatwork.co.uk>
2024-01-29 13:39:16 +0000

    Move steps under the dark line separator

--- resources/js/Components/DropDownItem.vue
+++ resources/js/Components/DropDownItem.vue
@@ -44,16 +44,19 @@ function handleAutosave(val){
 
 <template>
     <div class="bg-aaron-900 sm:rounded-lg p-8 h-full" v-show="tabIndex == selectedMenuId">
-        <div class="w-full flex flex-col gap-6 mb-14">
+        <div class="w-full flex flex-col gap-6">
             <div class="w-full flex flex-row items-center justify-between">
                 <h1 class="text-2xl font-medium">
-                    {{ tab.name }}
+                    {{ tab.name }} Chau
                 </h1>
                 <AutoSaveSpinner :autosave="autosaveState" />
             </div>
+            <hr class="-mx-32 h-1 mt-2 mb-6 bg-aaron-950 border-0">
+            <!--
             <div class="flex w-full h-2.5 overflow-hidden bg-gray-700 rounded-md">
                 <div class="bg-aaron-400 w-[50%] rounded-r-md" />
             </div>
+            -->
         </div>
         <div class="block">
             <SectionSidebar v-if="tab.sidebaritems" :sidebarItems="tab.sidebaritems">
