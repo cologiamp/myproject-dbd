5cd7ab9b6b5b87b5075715a7446dbbd3306f58ca
Harry Messenger <harrymessenger@Harrys-MacBook-Air.local>
2024-01-30 16:14:13 +0000

    today progess ah-127

---
+++ resources/js/currency.js
@@ -0,0 +1,42 @@
+import {ref} from "vue";
+import {useThrottleFn} from "@vueuse/core";
+import Swal from "sweetalert2";
+import {usePage} from "@inertiajs/vue3";
+
+export let autoS = ref(1);
+
+export const autosaveT = useThrottleFn((form, submitUrl)=>{
+    autosave(form, submitUrl)
+},2000)
+
+
+export function autosave(form,submitUrl){
+    autoS.value = 2;
+    form.transform((data)=>({
+        ...data,
+        autosave: true
+    })).put(submitUrl,
+        {
+            preserveScroll: true,
+            preserveState: true,
+            onSuccess: () => {
+                //Autosave actually takes a fraction of a second, but we delay to 1s to make it clear to user it actually happened
+                setTimeout(() => autoS.value = 1,1000);
+            },
+            onError:() => {
+                //Chore: Make this work
+                setTimeout(() => autoS.value = 3,1000);
+            }
+        }
+    )
+}
+
+export function __error(field, errorBag = 'default') {
+    if (!usePage().props.errors.hasOwnProperty(errorBag)) {
+        return null;
+    }
+    if (usePage().props.errors[errorBag].hasOwnProperty(field)) {
+        return usePage().props.errors[errorBag][field][0];
+    }
+    return null;
+}
