20cc9e30d366e50af2456c8184580748ecfc531e
Harry Messenger <harrymessenger@Harrys-MacBook-Air.local>
2024-02-06 10:39:00 +0000

    ah-100 changed negative margin to stop overflow on desktop

--- resources/js/Components/Tab.vue
+++ resources/js/Components/Tab.vue
@@ -51,7 +51,7 @@ function handleAutosave(val){
                 </h1>
                 <AutoSaveSpinner :autosave="autosaveState" />
             </div>
-            <hr class="-mx-32 h-1 mt-2 mb-6 bg-aaron-950 border-0">
+            <hr class="-mx-16 h-1 mt-2 mb-6 bg-aaron-950 border-0">
 
             <span class="-mb-3">{{ tab.name + ' progress: ' + tab.progress + '%' }} </span>
             <div class="flex w-full h-2.5 overflow-hidden bg-gray-700 rounded-md">
