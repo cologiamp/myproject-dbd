09b9cd9dd3ff18cb4e75a7a8915e97ba4c895a2d
genaquinoo <genetess.aquino@wealthatwork.co.uk>
2024-02-06 09:58:03 +0000

    AH-132: set default images sizes

--- resources/js/Pages/2FA.vue
+++ resources/js/Pages/2FA.vue
@@ -48,51 +48,51 @@ onMounted( ()=> {
         <div class="w-1/2">
             <div class="h-screen flex justify-center">
                 <div class="z-0 absolute top-1/2 transform -translate-y-1/2">
-                    <img src="/images/MAN1-550.png" id="round-elem-1" class="2xl:w-[450px] 2xl:h-[450px] 3xl:w-[550px] 3xl:h-[550px]"/>
+                    <img src="/images/MAN1-550.png" id="round-elem-1" class="2xl:w-[450px] 2xl:h-[450px] w-[550px] h-[550px]"/>
                 </div> 
                 <!-- top group elements -->
                 <div class="z-20 absolute top-[35%] right-[40%] transform -translate-x-[38%] -translate-y-full">
-                    <img src="/images/LOCK.svg" id="round-elem-2" class="2xl:w-[73px] 2xl:h-[73px] 3xl:w-[83px] 3xl:h-[83px]"/>
+                    <img src="/images/LOCK.svg" id="round-elem-2" class="2xl:w-[73px] 2xl:h-[73px] w-[83px] h-[83px]"/>
                 </div>
                 <div class="z-20 absolute top-[20%] right-[33%] transform -translate-x-[38%] -translate-y-full">
-                    <img src="/images/LADY1-150.png" id="round-elem-3" class="2xl:w-[140px] 2xl:h-[140px] 3xl:w-[150px] 3xl:h-[150px]"/>
+                    <img src="/images/LADY1-150.png" id="round-elem-3" class="2xl:w-[140px] 2xl:h-[140px] w-[150px] h-[150px]"/>
                 </div>
                 <div class="z-20 absolute top-[12%] right-[25%] transform -translate-x-[38%] -translate-y-full">
-                    <img src="/images/LADY2-85.png" id="round-elem-4" class="2xl:w-[75px] 2xl:h-[75px] 3xl:w-[85px] 3xl:h-[85px]"/>
+                    <img src="/images/LADY2-85.png" id="round-elem-4" class="2xl:w-[75px] 2xl:h-[75px] w-[85px] h-[85px]"/>
                 </div>
                 <div class="z-20 absolute top-[18%] right-[20%] transform -translate-x-[38%] -translate-y-full">
-                    <img src="/images/GRAPH-56.svg" id="round-elem-5" class="2xl:w-[46px] 2xl:h-[46px] 3xl:w-[56px] 3xl:h-[56px]"/>
+                    <img src="/images/GRAPH-56.svg" id="round-elem-5" class="2xl:w-[46px] 2xl:h-[46px] w-[56px] h-[56px]"/>
                 </div>
                 <div class="z-20 absolute top-[23%] right-[9%] transform -translate-x-[38%] -translate-y-full">
-                    <img src="/images/LADY3-135.png" id="round-elem-6" class="2xl:w-[125px] 2xl:h-[125px] 3xl:w-[135px] 3xl:h-[135px]"/>
+                    <img src="/images/LADY3-135.png" id="round-elem-6" class="2xl:w-[125px] 2xl:h-[125px] w-[135px] h-[135px]"/>
                 </div>
                 <div class="z-20 absolute top-[35%] right-[6%] transform -translate-x-[38%] -translate-y-full">
-                    <img src="/images/MATHS-SYMBOLS.svg" id="round-elem-7" class="2xl:w-[56px] 2xl:h-[56px] 3xl:w-[66px] 3xl:h-[66px]"/>
+                    <img src="/images/MATHS-SYMBOLS.svg" id="round-elem-7" class="2xl:w-[56px] 2xl:h-[56px] w-[66px] h-[66px]"/>
                 </div>
                 <div class="z-20 absolute top-[34%] right-[23%] transform -translate-x-[38%] -translate-y-full">
-                    <img src="/images/GRAPH1-184x122.svg" id="graph-elem-1" class="2xl:w-[174px] 2xl:h-[112px] 3xl:w-[184px] 3xl:h-[122px]"/>
+                    <img src="/images/GRAPH1-184x122.svg" id="graph-elem-1" class="2xl:w-[174px] 2xl:h-[112px] w-[184px] h-[122px]"/>
                 </div>
                 <!-- bottom group elements -->
                 <div class="z-10 absolute bottom-[30%] left-1/2 transform -translate-x-1/2 -translate-y-[65%]">
-                    <img src="/images/BULB-68.svg" id="round-elem-8" class="2xl:w-[58px] 2xl:h-[58px] 3xl:w-[68px] 3xl:h-[68px]"/>
+                    <img src="/images/BULB-68.svg" id="round-elem-8" class="2xl:w-[58px] 2xl:h-[58px] w-[68px] h-[68px]"/>
                 </div>
                 <div class="z-20 absolute bottom-[8%] right-[37%] transform -translate-x-[38%] -translate-y-full">
-                    <img src="/images/MAN2-124.png" id="round-elem-9" class="2xl:w-[114px] 2xl:h-[114px] 3xl:w-[123px] 3xl:h-[124px]"/>
+                    <img src="/images/MAN2-124.png" id="round-elem-9" class="2xl:w-[114px] 2xl:h-[114px] w-[123px] h-[124px]"/>
                 </div>
                 <div class="z-20 absolute bottom-[8%] right-[32%] transform -translate-x-[38%] -translate-y-full">
-                    <img src="/images/LADY4-66.png" id="round-elem-10" class="2xl:w-[56px] 2xl:h-[56px] 3xl:w-[66px] 3xl:h-[66px]"/>
+                    <img src="/images/LADY4-66.png" id="round-elem-10" class="2xl:w-[56px] 2xl:h-[56px] w-[66px] h-[66px]"/>
                 </div>
                 <div class="z-20 absolute -bottom-20 right-[20%] transform -translate-x-[38%] -translate-y-full">
-                    <img src="/images/MAN3-134.png" id="round-elem-11" class="2xl:w-[124px] 2xl:h-[124px] 3xl:w-[134px] 3xl:h-[134px]"/>
+                    <img src="/images/MAN3-134.png" id="round-elem-11" class="2xl:w-[124px] 2xl:h-[124px] w-[134px] h-[134px]"/>
                 </div>
                 <div class="z-20 absolute bottom-[8%] right-[15%] transform -translate-x-[38%] -translate-y-full">
-                    <img src="/images/CURSOR-45.svg" id="round-elem-12" class="2xl:w-[35px] 2xl:h-[35px] 3xl:w-[45px] 3xl:h-[45px]"/>
+                    <img src="/images/CURSOR-45.svg" id="round-elem-12" class="2xl:w-[35px] 2xl:h-[35px] w-[45px] h-[45px]"/>
                 </div>
                 <div class="z-20 absolute bottom-[2%] right-[7%] transform -translate-x-[38%] -translate-y-full">
-                    <img src="/images/MAN4-78.png" id="round-elem-13" class="2xl:w-[68px] 2xl:h-[68px] 3xl:w-[78px] 3xl:h-[78px]"/>
+                    <img src="/images/MAN4-78.png" id="round-elem-13" class="2xl:w-[68px] 2xl:h-[68px] w-[78px] h-[78px]"/>
                 </div>
                 <div class="z-20 absolute bottom-[7%] right-[5%] transform -translate-x-[38%] -translate-y-full">
-                    <img src="/images/GRAPH2-184x122.svg" id="graph-elem-2" class="2xl:w-[174px] 2xl:h-[112px] 3xl:w-[184px] 3xl:h-[122px]"/>
+                    <img src="/images/GRAPH2-184x122.svg" id="graph-elem-2" class="2xl:w-[174px] 2xl:h-[112px] w-[184px] h-[122px]"/>
                 </div>
             </div>
         </div>
