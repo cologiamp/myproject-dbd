a3f6484d1613f826047098e4755304daf242501b
Paul Ingram <paul@pmingram.co.uk>
2024-03-19 07:58:17 +0000

    Skeleton for egress job

--- app/Jobs/DataEgress/UpdateClientEmploymentJob.php
+++ app/Jobs/DataEgress/UpdateClientEmploymentJob.php
@@ -2,6 +2,8 @@
 
 namespace App\Jobs\DataEgress;
 
+use App\Models\Client;
+use App\Services\DataEgressService;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -12,11 +14,19 @@ class UpdateClientEmploymentJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public function __construct()
+    private DataEgressService $dataEgressService;
+
+    public function __construct(private Client $client)
     {
+        $this->dataEgressService = new DataEgressService();
     }
 
     public function handle(): void
     {
+        $this->dataEgressService->updateClientData($this->client);
+        $this->dataEgressService->updateClientContacts($this->client);
+        $this->dataEgressService->updateEmployment($this->client);
+        $this->dataEgressService->updateDependents($this->client);
+        $this->dataEgressService->updateAddresses($this->client);
     }
 }
