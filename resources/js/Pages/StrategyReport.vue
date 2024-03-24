32df158360bf420e7e528c8d8e23212c0f0bdac1
Ignacio Giampaoli <ignacio.giampaoli@wealthatwork.co.uk>
2024-03-24 23:18:12 +0000

    Adding table style to list of pdf files

--- resources/js/Pages/StrategyReport.vue
+++ resources/js/Pages/StrategyReport.vue
@@ -1,6 +1,7 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
 import FormWell from "@/Components/FormWell.vue";
+import draggable from "vuedraggable";
 
 import {
     ArrowRightIcon
@@ -8,6 +9,8 @@ import {
 import Swal from "sweetalert2";
 import {onMounted, ref} from "vue";
 import {isMobile} from "@/mobileCheck.js";
+import {ArrowsUpDownIcon, PencilSquareIcon, TrashIcon} from "@heroicons/vue/24/outline/index.js";
+import {Link} from "@inertiajs/vue3";
 
 const props = defineProps({
     title: String,
@@ -55,12 +58,63 @@ function generateReport(){
                                 </button>
                             </div>
                         </div>
-                        <div class="p-4 mt-4 bg-aaron-400">
-                            <ul class="p-4 bg-aaron-950" v-for="report in stratReportRef">
-                                <li class="bg-pink-700 text-aaron-50 p-5">
-                                    {{report}}
-                                </li>
-                            </ul>
+                        <div class="p-4 mt-4">
+
+                            <div class="mt-8 flow-root">
+                                <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                                    <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
+                                        <table class="min-w-full divide-y divide-gray-500">
+                                            <thead class="bg-aaron-700">
+                                            <tr>
+                                                <th scope="col" class="p-3"></th>
+                                                <th scope="col" class="px-3 py-3.5 text-left font-semibold">Date</th>
+                                                <th scope="col" class="px-3 py-3.5 text-left font-semibold">File</th>
+                                                <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0"></th>
+                                                <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0"></th>
+                                            </tr>
+                                            </thead>
+                                            <draggable v-if="stratReportRef.length > 0" v-model="stratReportRef" tag="tbody" item-key="name" @change="updateOrder" class="divide-y divide-gray-500">
+                                                <template #item="{ element }">
+                                                    <tr>
+                                                        <td class="px-3 py-4">
+                                                            <!--
+                                                            <a href="#" class="hover:text-indigo-900">
+                                                                <ArrowsUpDownIcon class="w-5 h-5"></ArrowsUpDownIcon>
+                                                            </a>
+                                                            -->
+                                                        </td>
+                                                        <td scope="row" class="text-sm px-3 py-4">{{ element.date }}</td>
+                                                        <td class="text-sm px-3 py-4">
+                                                            <Link :href="element.file">
+                                                                {{ element.file }}
+                                                            </Link>
+                                                        </td>
+                                                        <td class="relative text-sm py-4 pl-3 pr-4 text-right sm:pr-0">
+                                                            <a href="#" @click="edit(element.id)">
+                                                                <PencilSquareIcon class="w-5 h-5"></PencilSquareIcon>
+                                                            </a>
+                                                        </td>
+                                                        <td class="relative text-sm py-4 pl-3 pr-4 text-right sm:pr-0">
+                                                            <a href="#" @click="removeReport(element.id)">
+                                                                <TrashIcon class="w-5 h-5"></TrashIcon>
+                                                            </a>
+                                                        </td>
+                                                    </tr>
+                                                </template>
+                                            </draggable>
+                                            <tbody v-else>
+                                            <tr class="text-center">
+                                                <td colspan="7" class="text-sm px-3 py-4 text-center">
+                                                    No objectives data.
+                                                </td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                                <rawDisplayer class="col-3" :value="list" title="List" />
+                            </div>
+
                         </div>
                     </div>
                 </div>
