6e74f0377ffdd87df2da04892cfd87d5db37b0b4
Piers Vaughan <piersvaughan@Pierss-MacBook-Pro-16.local>
2024-03-28 16:22:55 +0000

    Bugfixes

--- app/Http/Controllers/RiskProfileQuestionnaireController.php
+++ app/Http/Controllers/RiskProfileQuestionnaireController.php
@@ -28,33 +28,39 @@ class RiskProfileQuestionnaireController extends Controller
         $this->riskProfileRepository->setClient($client);
 
         return Inertia::render('RiskProfileQuestionnaire',[
-            'client_id' => $client->id
+            'client_id' => $client->io_id
         ]);
     }
-    public function submitQ1(Request $request): \Illuminate\Http\RedirectResponse
+    public function submitQ1(Client $client,Request $request): \Illuminate\Http\RedirectResponse
     {
+        $this->clientRepository->setClient($client);
+        $this->riskProfileRepository->setClient($client);
         $request->validate([
             'risk_tolerance' => 'required|boolean',
         ]);
 
         $this->saveData('comfort_fluctuate_market', $request->risk_tolerance);
 
-        return to_route('client.risk.front');
+        return to_route('client.risk.front', ['client' => $client->io_id]);
     }
 
     public function submitQ2(Client $client, Request $request): \Illuminate\Http\RedirectResponse
     {
+        $this->clientRepository->setClient($client);
+        $this->riskProfileRepository->setClient($client);
         $request->validate([
             'volatility' => 'required|integer',
         ]);
 
         $this->saveData('day_to_day_volatility', $request->volatility, $client);
 
-        return to_route('client.risk.front');
+        return to_route('client.risk.front', ['client' => $client->io_id]);
     }
 
-    public function submitQ3(Request $request): \Illuminate\Http\RedirectResponse
+    public function submitQ3(Client $client,Request $request): \Illuminate\Http\RedirectResponse
     {
+        $this->clientRepository->setClient($client);
+        $this->riskProfileRepository->setClient($client);
         $request->validate([
             'amount_invested' => 'required|numeric',
             'concern5percent' => 'required|numeric',
@@ -76,51 +82,59 @@ class RiskProfileQuestionnaireController extends Controller
 
         $this->saveData('short_term_volatility', json_encode($short_term_volatility));
 
-        return to_route('client.risk.front');
+        return to_route('client.risk.front', ['client' => $client->io_id]);
     }
 
-    public function submitQ4(Request $request): \Illuminate\Http\RedirectResponse
+    public function submitQ4(Client $client,Request $request): \Illuminate\Http\RedirectResponse
     {
+        $this->clientRepository->setClient($client);
+        $this->riskProfileRepository->setClient($client);
         $request->validate([
             'comfortable_range' => 'required|integer',
         ]);
 
         $this->saveData('medium_term_volatility', $request->comfortable_range);
 
-        return to_route('client.risk.front');
+        return to_route('client.risk.front', ['client' => $client->io_id]);
     }
 
-    public function submitQ5(Request $request): \Illuminate\Http\RedirectResponse
+    public function submitQ5(Client $client, Request $request): \Illuminate\Http\RedirectResponse
     {
+        $this->clientRepository->setClient($client);
+        $this->riskProfileRepository->setClient($client);
         $request->validate([
             'value_drop_behaviour' => 'required|integer',
         ]);
 
         $this->saveData('volatility_behaviour', $request->value_drop_behaviour);
 
-        return to_route('client.risk.front');
+        return to_route('client.risk.front', ['client' => $client->io_id]);
     }
 
-    public function submitQ6(Request $request): \Illuminate\Http\RedirectResponse
+    public function submitQ6(Client $client, Request $request): \Illuminate\Http\RedirectResponse
     {
+        $this->clientRepository->setClient($client);
+        $this->riskProfileRepository->setClient($client);
         $request->validate([
             'comfortable_volatility' => 'required|integer',
         ]);
 
         $this->saveData('long_term_volatility', $request->comfortable_volatility);
 
-        return to_route('client.risk.front');
+        return to_route('client.risk.front', ['client' => $client->io_id]);
     }
 
-    public function submitQ7(Request $request): \Illuminate\Http\RedirectResponse
+    public function submitQ7(Client $client, Request $request): \Illuminate\Http\RedirectResponse
     {
+        $this->clientRepository->setClient($client);
+        $this->riskProfileRepository->setClient($client);
         $request->validate([
             'cash_in_behaviour' => 'required|integer',
         ]);
 
         $this->saveData('time_in_market', $request->cash_in_behaviour);
 
-        return to_route('client.risk.front');
+        return to_route('client.risk.front', ['client' => $client->io_id]);
     }
 
     private function saveData($fieldName, $fieldValue){
